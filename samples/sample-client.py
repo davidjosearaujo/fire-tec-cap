11420272d8e7bdb8ded2240279667c3094c4e119
David Araújo <david2araujo5@gmail.com>
2023-02-02 12:37:13 +0000

    Update

--- samples/sample-client.py
+++ samples/sample-client.py
@@ -1,22 +1,23 @@
 import capparser
 
-alert = capparser.element.Alert(sender="FireTec", 
+alert = capparser.element.Alert(sender="FireTec",
                                 status=capparser.enums.Status.Actual,
                                 msgType=capparser.enums.MsgType.Alert,
                                 scope=capparser.enums.Scope.Private)
 alert.setSource("FireTec")
-alert.addAddress("Radio Renascença")
+alert.addAddress("Radio Renascenca")
 
 info = capparser.element.Info(category=[capparser.enums.Category.Fire],
-                        event="Incendio Florestal em Sever do Vouga",
-                        urgency=capparser.enums.Urgency.Immediate,
-                        severity=capparser.enums.Severity.Severe,
-                        certainty=capparser.enums.Certainty.Observed)
+                              event="Incendio Florestal em Sever do Vouga",
+                              urgency=capparser.enums.Urgency.Immediate,
+                              severity=capparser.enums.Severity.Severe,
+                              certainty=capparser.enums.Certainty.Observed)
 info.setSenderName("FireTec")
-info.setInstruction("Foi detetado um possivel incendio florestal na sua area, precaucao e aconselhada.")
+info.setInstruction(
+    "Foi detetado um possivel incendio florestal na sua area, precaucao e aconselhada.")
 
-param1 = capparser.element.Parameter(parameterName="PS", 
-                                     parameterValue="PS=Radio Renascença")
+param1 = capparser.element.Parameter(parameterName="PS",
+                                     parameterValue="PS=Radio Renascenca")
 param2 = capparser.element.Parameter(parameterName="PI",
                                      parameterValue="PI=80XX")
 param3 = capparser.element.Parameter(parameterName="AF",
@@ -34,4 +35,4 @@ info.addParameter(param5)
 
 alert.addInfo(info)
 
-capparser.deparse(alert)
+capparser.writeAlertToFile(alert, "output.xml")
