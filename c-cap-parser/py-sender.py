9d7ccd99cb325fe7cb3d4328c41c0b01f10ec5be
David Araújo <david2araujo5@gmail.com>
2023-02-18 21:12:49 +0000

    Update

--- c-cap-parser/py-sender.py
+++ c-cap-parser/py-sender.py
@@ -3,11 +3,10 @@ import socket
 import capparser
 
 alert = capparser.parse(filePath="../FireTec_Example.xml")
-print(capparser.deparse(alert))
-b = bytes(capparser.deparse(alert))
 
 HOST = 'localhost'  # The remote host
 PORT = 9090         #The same port as used by the server
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b)
+    # Alert object must be converted into a string and then to bytes
+    s.sendall(bytes(capparser.deparse(alert)))
